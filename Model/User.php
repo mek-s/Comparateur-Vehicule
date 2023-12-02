7a70018b32b2f18893e8912fff941bf7c7aac042
Soumeya <ks_mekki@esi.dz>
2023-12-02 18:48:21 +0100

    user model

---
+++ Model/User.php
@@ -0,0 +1,21 @@
+<?php
+require_once('controller.php');
+require_once('bdd.php');
+
+class userModel{
+
+    private $db ;
+    
+    $this->db = new bdd();
+    $cnx = $this->db->connect();
+
+    
+
+
+
+
+    $this->$db.disconnect();
+
+}
+
+?>
