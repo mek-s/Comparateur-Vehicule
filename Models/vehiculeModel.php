7d9724492662e66397216eed250bbb4db580ee20
Soumeya <ks_mekki@esi.dz>
2023-12-22 22:25:42 +0100

    vehicules list + principal vehicules

--- Models/vehiculeModel.php
+++ Models/vehiculeModel.php
@@ -20,6 +20,36 @@ class vehiculeModel{
     
         $this->db->disconnect($cnx);
     }
+
+    public function getPrincipalesVehiculesModel($params){
+        $cnx=$this->db->connect();
+
+        $query = "SELECT i.chemin , veh.vehicule_id FROM `images` i JOIN( SELECT vh.image_id , vh.vehicule_id FROM `vehicules` vh JOIN( SELECT v.version_id FROM `versions` v JOIN ( SELECT md.modele_id FROM `modeles` md JOIN `marques` mr ON md.marque_id = mr.marque_id WHERE (mr.marque_id= ? AND md.supp= 0 AND mr.supp= 0) ) m ON v.modele_id = m.modele_id WHERE v.supp = 0 ) v ON v.version_id=vh.version_id WHERE vh.supp=0 AND vh.principal=1 ) veh ON veh.image_id=i.image_id";
+        $vehicules =$this->db->request($cnx,$query,$params);
+    
+        $this->db->disconnect($cnx);
+        return $vehicules;
+    }
+
+    public function getVehiculeCracteristiques($params){
+        $cnx=$this->db->connect();
+
+        $query = "SELECT value,carac_nom FROM `carac_vehicule` NATURAL JOIN `vehicules` NATURAL JOIN `caracteristiques` WHERE vehicule_id= ?";
+        $caracs=$this->db->request($cnx,$query,$params);
+    
+        $this->db->disconnect($cnx);
+        return $caracs;
+    }
+
+    public function getMarquesVehiculesModel($params){
+        $cnx=$this->db->connect();
+
+        $query = "SELECT i.chemin , veh.* FROM `images` i JOIN( SELECT vh.* , v.version_nom , v.modele_nom FROM `vehicules` vh JOIN( SELECT v.* , m.modele_nom FROM `versions` v JOIN ( SELECT md.* ,mr.marque_nom FROM `modeles` md JOIN `marques` mr ON md.marque_id = mr.marque_id WHERE (mr.marque_id= ? AND md.supp= 0 AND mr.supp= 0) ) m ON v.modele_id = m.modele_id WHERE v.supp = 0 ) v ON v.version_id=vh.version_id WHERE vh.supp=0 ) veh ON veh.image_id=i.image_id";
+        $vehicules = $this->db->request($cnx,$query,$params);
+    
+        $this->db->disconnect($cnx);
+        return $vehicules;
+    }
 }
 
 
--- Views/marqueView.php
+++ Views/marqueView.php
@@ -1,14 +1,18 @@
 <?php
 require_once "C:\wamp64\www\Comparateur-Vehicule\Controllers\marqueController.php";
 require_once "C:\wamp64\www\Comparateur-Vehicule\Views\userViews\homeView.php";
+require_once "C:\wamp64\www\Comparateur-Vehicule\Models\/vehiculeModel.php";
 
 class marqueView{
 
     private $controller;
-    private $chemin;
+    private $model;
+    private $cheminM;
+    private $cheminV;
 
     public function __construct() {
-      $this->chemin = $GLOBALS['base-url'] . 'Images/marques/';
+      $this->cheminM = $GLOBALS['base-url'] . 'Images/marques/';
+      $this->cheminV = $GLOBALS['base-url'] . 'Images/vehicules/';
     }
 
     public function createMarqueView(){
@@ -39,7 +43,7 @@ class marqueView{
       foreach ($principales as $marque) {?>
        <div class="marque<?php $marque['marque_id']?>">
         <a href="/Comparateur-Vehicule/marques/details?marque=<?php echo $marque['marque_id']?>">
-          <img src="<?php echo $this->chemin.$marque['chemin']; ?>" alt="">
+          <img src="<?php echo $this->cheminM.$marque['chemin']; ?>" alt="">
         </a>
         </div>
          
@@ -52,10 +56,13 @@ class marqueView{
       <div class="marques-zone">
       <?php foreach ($marques as $marque) { ?>
 
-         <div class="marque" id="marque<?php$marque['marque_id']?>">
-          <a href="/Comparateur-Vehicule/marques/details?marque=<?php echo $marque['marque_id']?>">
-            <img src="<?php echo $this->chemin.$marque['chemin'] ;?>" alt="">
-          </a>
+         <div class="marque" id="marque<?php echo $marque['marque_id'];?>">
+            
+            <a href="/Comparateur-Vehicule/marques/details?marque=<?php echo $marque['marque_id']?>">
+              <img src="<?php echo $this->cheminM.$marque['chemin'] ;?>" alt="">
+            </a>
+            
+            <span><?php echo $marque['marque_nom'];?></span>
          </div>
 
       <?php
@@ -67,12 +74,17 @@ class marqueView{
 
       require_once("C:\wamp64\www\Comparateur-Vehicule\Views\userViews\header.php");
       $home = new homeView();
+      $this->model = new vehiculeModel();
+
       $home->showMenu();
+
+      $vehicules = $this->model->getMarquesVehiculesModel(array(1 => $marque['marque_id']));
+      
       ?>
       <div class="details-container">
         <div class="marque-details">
           <div class="marque">
-            <img src="<?php echo $this->chemin.$marque['chemin']; ?>">
+            <img src="<?php echo $this->cheminM.$marque['chemin']; ?>">
           </div>
           <div class="infos">
             <h3>La marque <?php echo $marque['marque_nom']; ?></h3>
@@ -86,12 +98,46 @@ class marqueView{
           </div>
           
         </div>
+        <h1>Vehicules Principales</h1>
         <div class="vehic-principals">
-           <h1>Vehicules Populaires</h1>
+          <?php
+           $Pvehicules = $this->model->getPrincipalesVehiculesModel(array(1 => $marque['marque_id']));
+           foreach ($Pvehicules as $vehicule) {?>
+            <div class="principals-images">
+             <a href="/Comparateur-Vehicule/vehicules/details?vehicule=<?php echo $vehicule['vehicule_id']?>">
+               <img src="<?php echo $this->cheminV.$vehicule['chemin']; ?>" alt="">
+             </a>
+             </div>
+              
+            <?php
+           }
+          ?> 
         </div>
         <div class="vehicules">
-         <h1>Vehicules de la marques<?php echo $marque['marque_nom']; ?></h1>
-         
+         <h1>Vehicules de la marques <?php echo $marque['marque_nom']; ?></h1>
+          <?php
+           foreach ($vehicules as $vehicule) {?>
+            <div class="vehicule">
+              <a href="/Comparateur-Vehicule/vehicules/details?vehicule=<?php echo $vehicule['vehicule_id']?>">
+               <img src="<?php echo $this->cheminV.$vehicule['chemin'];?>">
+              </a>
+              <div class="details">
+                <p>Nom : <?php echo $vehicule['vehicule_nom']; ?></p>
+                <p>Modele: <?php echo $vehicule['modele_nom']; ?></p>
+                <p>Version :<?php echo $vehicule['version_nom']; ?></p>
+              </div>
+              <div class="caracteristiques">
+              <?php
+                 $caracteristiques = $this->model->getVehiculeCracteristiques(array(1 => $vehicule['vehicule_id']));
+
+                 foreach ($caracteristiques as $carac) {?>
+                   <p><?php echo $carac['carac_nom'].':'.$carac['value']; ?> </p>
+                 <?php }
+              ?>
+            </div>
+         </div>
+          <?php }
+          ?>
         </div>
       </div>
       <?php 
